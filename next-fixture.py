852079277f71e8679af59fbeb18ccea2e9faf3e6
Rob <rob.edwards101@googlemail.com>
2017-02-26 20:29:46 +0000

    Added user input

--- next-fixture.py
+++ next-fixture.py
@@ -1,9 +1,29 @@
-import urllib
+# TO DO:
+# Refactor to use json library for nicer data scraping
+# Get previous form (difficult!!!)
 
-def get_next_fixture():
-    feed = urllib.urlopen("http://www.footballwebpages.co.uk/matches.json?team=70&fixtures=5&results=0")
+import urllib
+import sys
+
+def get_team(team):
+    # Pass team name in and then return the team number
+    feed = urllib.urlopen("http://www.footballwebpages.co.uk/teams.json")
     page = feed.read()
-    info = ["date", "competition", "status", "homeTeamName", "awayTeamName"]
+    marker = page.find(team)
+    if marker == -1:
+        return marker
+    snippet = page[marker-25:marker]
+    team_num_start = snippet.find(":")
+    team_num_end = snippet.find('"', team_num_start+3)
+    team_num = snippet[team_num_start+3:team_num_end]
+    return team_num
+
+def get_next_fixture(num):
+    # Takes team number and inserts it in URL to get the latest fixtures
+    url = "http://www.footballwebpages.co.uk/matches.json?team=" + str(num) + "&fixtures=5&results=0" 
+    feed = urllib.urlopen(url)
+    page = feed.read()
+    info = ["date", "competition", "status", "homeTeamName", "awayTeamName", "homeTeamNo"]
     i = 0
     for element in info:
         start_element = page.find(element)
@@ -11,11 +31,17 @@ def get_next_fixture():
         end_info = page.find('"', start_info+3)
         info[i] = page[start_info+3:end_info]
         i += 1
+    team_name = home_or_away(num, info)
+    return info, team_name
 
-    return info
+def home_or_away(n, p):
+    if p[5] == n:
+        return p[3]
+    else:
+        return p[4]
 
-def format_output(p):
-    output = """    Bristol Rovers next game:
+def format_output(p, name):
+    output = name + """'s next game:
     ----------------------
     """ + p[1] + """
     """ + p[0] + """, """ + p[2] + """
@@ -23,6 +49,16 @@ def format_output(p):
               
     return output
 
+def generate_fixture():
+     
+     #a = get_team(sys.argv[1])
+     a = get_team("Bristol City")
+     if a != -1:
+         p, team = get_next_fixture(a)
+         print format_output(p, team)
+     else:
+         print "Team unknown"
+      
 
 
-print format_output(get_next_fixture())
+generate_fixture()
